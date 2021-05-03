dbfa09141e0a09f9c9ed5dd621a14da63b626239
jmmp8 <jajamao11@gmail.com>
2021-05-03 13:37:39 -0500

    add test for log in button click and create eslint config

--- src/app/login-button/login-button.component.spec.ts
+++ src/app/login-button/login-button.component.spec.ts
@@ -1,4 +1,9 @@
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonHarness} from '@angular/material/button/testing';
+
 import {AuthService} from '../auth.service';
 import {AuthServiceStub} from '../testing/auth-service-stub';
 
@@ -9,13 +14,16 @@ describe('LoginButtonComponent', () => {
   let compiled: HTMLElement;
   let component: LoginButtonComponent;
   let fixture: ComponentFixture<LoginButtonComponent>;
-
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     authServiceStub = new AuthServiceStub();
 
     await TestBed
         .configureTestingModule({
+          imports: [
+            MatButtonModule,
+          ],
           declarations: [LoginButtonComponent],
           providers: [{provide: AuthService, useValue: authServiceStub}],
         })
@@ -26,6 +34,7 @@ describe('LoginButtonComponent', () => {
     fixture = TestBed.createComponent(LoginButtonComponent);
     component = fixture.componentInstance;
     compiled = fixture.nativeElement;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
@@ -33,24 +42,33 @@ describe('LoginButtonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show a log out button and display the user\'s email', () => {
-    const logInButton = compiled.querySelector('.login-button');
-    if (!logInButton) throw new Error('Failed to find log in button');
-    expect(logInButton.textContent).toContain('Log Out');
+  it('should show a log out button and display the user\'s email', async () => {
+    const logInButton = await loader.getHarness(
+        MatButtonHarness.with({selector: '.login-button'}));
+    expect(await logInButton.getText()).toContain('Log Out');
 
     const emailLabel = compiled.querySelector('.user-email-label');
     if (!emailLabel) throw new Error('Failed to find user email label');
-    let expectedEmail = authServiceStub.getUserEmail();
+    const expectedEmail = authServiceStub.getUserEmail();
     expect(emailLabel.textContent).toContain(expectedEmail);
   });
 
-  it('should show a login button if the user is not logged in', () => {
+  it('should show a login button if the user is not logged in', async () => {
     authServiceStub.user = null;
     fixture.detectChanges();
 
-    const logInButton = compiled.querySelector('.login-button');
-    if (!logInButton) throw new Error('Failed to find log in button');
+    const logInButton = await loader.getHarness(
+        MatButtonHarness.with({selector: '.login-button'}));
+    expect(await logInButton.getText()).toContain('Log In');
+  });
 
-    expect(logInButton.textContent).toContain('Log In');
-  })
+  it('should attempt to log in or out when the button is clicked', async () => {
+    spyOn(authServiceStub, 'logInOrOut');
+
+    const logInButton = await loader.getHarness(
+        MatButtonHarness.with({selector: '.login-button'}));
+    await logInButton.click();
+
+    expect(authServiceStub.logInOrOut).toHaveBeenCalled();
+  });
 });
--- src/app/testing/auth-service-stub.ts
+++ src/app/testing/auth-service-stub.ts
@@ -1,5 +1,6 @@
 export class AuthServiceStub {
   user: string|null = 'test_user@test.com';
+
   async logInOrOut(): Promise<void> {}
 
   getUserEmail(): string|null {
