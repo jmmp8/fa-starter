041d2924ad021ec9621e2b63eb1b2f8f479336eb
jmmp8 <jajamao11@gmail.com>
2021-05-03 12:09:51 -0500

    clean up sql statement execution

--- db.py
+++ db.py
@@ -28,12 +28,30 @@ def connect():
     return conn
 
 
+def fetch_sql(sql, num_rows=None):
+    """Executes a SQL query and returns the results
+
+    Args:
+        sql (str): SQL query to run
+        num_rows (int, optional): The number of rows to query for.
+          Defaults to None, which will fetch all rows.
+
+    Returns:
+        List[Dict]: A list or the rows returned by the query as dictionaries.
+    """
+    conn = connect()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            if num_rows is not None:
+                results = cursor.fetchmany(num_rows)
+            else:
+                results = cursor.fetchall()
+            return results
+    finally:
+        conn.close()
+
+
 @blueprint.route('/test')
 def test():
-    conn = connect()
-    with conn.cursor() as cursor:
-        result = cursor.execute('select * from users')
-        users = cursor.fetchall()
-        if result > 0:
-            return flask.jsonify(users)
     return 'pong!'
