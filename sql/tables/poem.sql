c4fcd946e9a3a76c5d69a494f03f783cbec784ca
jmmp8 <jajamao11@gmail.com>
2021-05-07 16:11:15 -0500

    create poem sql table

---
+++ sql/tables/poem.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `poem` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `creation_timestamp` datetime NOT NULL,
+  `modified_timestamp` datetime DEFAULT NULL,
+  `privacy_level` enum('public','private') NOT NULL DEFAULT 'public',
+  `archived` bit(1) NOT NULL DEFAULT b'0',
+  `form` enum('haiku','sonnet','tonka') DEFAULT NULL,
+  `generated` bit(1) NOT NULL DEFAULT b'0',
+  `text` varchar(8000) NOT NULL,
+  `num_likes` int NOT NULL DEFAULT '0',
+  `num_dislikes` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `user_fk_idx` (`user_id`),
+  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
