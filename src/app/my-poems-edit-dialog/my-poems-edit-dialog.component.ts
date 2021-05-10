2f4d603cfc3e9da4a2a1ef0c912be491753fca2c
jmmp8 <jajamao11@gmail.com>
2021-05-10 12:49:31 -0500

    create my poem edit dialog

--- src/app/my-poems-edit-dialog/my-poems-edit-dialog.component.ts
+++ src/app/my-poems-edit-dialog/my-poems-edit-dialog.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-my-poems-edit-dialog',
   templateUrl: './my-poems-edit-dialog.component.html',
   styleUrls: ['./my-poems-edit-dialog.component.css']
 })
-export class MyPoemsEditDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MyPoemsEditDialogComponent {
+  submit(poemName: string, poemText: string): void {
+    console.log(poemName, poemText);
   }
 
+  canSubmit(poemName: string, poemText: string): boolean {
+    return (poemName.trim().length > 0) && (poemText.trim().length > 0);
+  }
+
+  cancel(): void {
+    console.log('canceling');
+  }
 }
--- src/app/my-poems-list/my-poems-list.component.ts
+++ src/app/my-poems-list/my-poems-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {Poem} from '../backend_response_types';
+import {MyPoemsEditDialogComponent} from '../my-poems-edit-dialog/my-poems-edit-dialog.component';
 
 @Component({
   selector: 'app-my-poems-list',
   templateUrl: './my-poems-list.component.html',
   styleUrls: ['./my-poems-list.component.css']
 })
-export class MyPoemsListComponent implements OnInit {
+export class MyPoemsListComponent {
+  constructor(public editDialog: MatDialog) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  openEditDialog(poem?: Poem): void {
+    const dialogRef = this.editDialog.open(MyPoemsEditDialogComponent);
   }
-
 }
