9749439be30951c58a397698f496dc33c05b31ab
jmmp8 <jajamao11@gmail.com>
2021-05-12 16:47:24 -0500

    create get poem endpoint for manual poems

--- api_test.py
+++ api_test.py
@@ -1,6 +1,7 @@
 """Tests for db.py"""
 import json
 from absl.testing import absltest
+from datetime import datetime, timedelta
 from flask_webtest import TestApp
 
 from app import app
@@ -41,6 +42,28 @@ class DbTest(absltest.TestCase):
         db.session.execute(wipe_user_sql)
         db.session.commit()
 
+    def populate_user_data(self):
+        user = models.User(email=self.test_email)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def populate_poem_data(self, user_id, generated, num_poems=1):
+        results = []
+        for i in range(num_poems):
+            poem = models.Poem(
+                user_id=user_id,
+                creation_timestamp=(datetime.now() -
+                                    timedelta(minutes=num_poems - i)),
+                generated=generated,
+                name=f'test poem {i}',
+                text=f'poem {i}\ntest\ntext',
+            )
+            db.session.add(poem)
+            results.append(poem)
+        db.session.commit()
+        return results
+
     @with_app_context
     def test_create_user(self):
         first_create = self.w.get(f'/api/create_user/{self.test_email}')
@@ -61,9 +84,7 @@ class DbTest(absltest.TestCase):
     @with_app_context
     def test_create_poem(self):
         # First create user to associate the poem with
-        user = models.User(email=self.test_email)
-        db.session.add(user)
-        db.session.flush()
+        user = self.populate_user_data()
 
         # Call the endpoint
         poem_name = 'test poem'
@@ -106,6 +127,39 @@ class DbTest(absltest.TestCase):
 
         with self.assertRaises(ValueError) as context:
             self.w.post_json('/api/create_poem', request_body)
+        self.assertTrue(
+            'Failed to find a user with email' in str(context.exception))
+
+    @with_app_context
+    def test_get_manual_poems(self):
+        # First create the user to associate poems with
+        user = self.populate_user_data()
+
+        # Create some poems to query for
+        num_poems = 4
+        all_poems = self.populate_poem_data(user.id, False, num_poems)
+        all_poems.sort(
+            key=lambda p: p.modified_timestamp or p.creation_timestamp)
+
+        # Query for all the manual poems
+        get = self.w.get(f'/api/get_poems/manual/0/{user.email}')
+        get_resp = json.loads(get.body)
+        self.assertEqual(get_resp['type'], 'manual')
+
+        all_manual_poems = get_resp['poems']
+        self.assertEqual(len(all_manual_poems), len(all_poems))
+        for i in range(num_poems):
+            poem = all_poems[i]
+            manual_poem = all_manual_poems[i]
+            self.assertEqual(poem.name, manual_poem['name'])
+            self.assertEqual(poem.text, manual_poem['text'])
+            self.assertEqual(manual_poem['generated'], False)
+            self.assertEqual(manual_poem['user_id'], user.id)
+
+    @with_app_context
+    def test_get_manual_poems_error(self):
+        with self.assertRaises(ValueError) as context:
+            self.w.get(f'/api/get_poems/manual/0/{self.test_email}')
         self.assertTrue(
             'Failed to find a user with email' in str(context.exception))
 
