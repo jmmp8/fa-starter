c1438cfa8f4c0775993b9fbbb4d958a025419992
jmmp8 <jajamao11@gmail.com>
2021-05-18 16:24:42 -0500

    hook up manual poem cards to edit poems

--- src/app/my-poems-edit-dialog/my-poems-edit-dialog.component.ts
+++ src/app/my-poems-edit-dialog/my-poems-edit-dialog.component.ts
@@ -13,6 +13,7 @@ import {MessagePopup} from '../message-popup/message-popup.component';
   styleUrls: ['./my-poems-edit-dialog.component.css']
 })
 export class MyPoemsEditDialog {
+  existingPoem: Poem;
   poemName = '';
   poemText = '';
 
@@ -21,25 +22,45 @@ export class MyPoemsEditDialog {
       private dialogRef: MatDialogRef<MyPoemsEditDialog>,
       @Inject(MAT_DIALOG_DATA) public data: Poem,
       private snackBar: MatSnackBar,
-  ) {}
+  ) {
+    if (data) {
+      this.existingPoem = data;
+      this.poemName = data.name;
+      this.poemText = data.text;
+    }
+  }
 
   canSubmit(): boolean {
-    return (this.poemName.trim().length > 0) &&
-        (this.poemText.trim().length > 0);
+    return (this.poemName.trim().length > 0) && (this.poemText.length > 0);
   }
 
   async submit(): Promise<void> {
     if (this.canSubmit()) {
-      this.snackBar.openFromComponent(
-          MessagePopup,
-          {
-            data: this.backendService
-                      .createPoem(this.poemName, this.poemText, false)
-                      .pipe(
-                          map(response => response ? 'Poem Created!' :
-                                                     'Failed to create poem.'))
-          },
-      );
+      if (this.existingPoem) {
+        this.existingPoem.name = this.poemName;
+        this.existingPoem.text = this.poemText;
+
+        this.snackBar.openFromComponent(
+            MessagePopup,
+            {
+              data: this.backendService.editPoem(this.existingPoem)
+                        .pipe(
+                            map(response => response ? 'Poem edit complete.' :
+                                                       'Failed to edit poem.'))
+            },
+        );
+      } else {
+        this.snackBar.openFromComponent(
+            MessagePopup,
+            {
+              data: this.backendService
+                        .createPoem(this.poemName, this.poemText, false)
+                        .pipe(map(
+                            response => response ? 'Poem Created!' :
+                                                   'Failed to create poem.'))
+            },
+        );
+      }
     }
     this.dialogRef.close();
   }
